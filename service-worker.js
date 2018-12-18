92654d2d2de7a4a19c6c0db48488b0ca6f74cf00
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-18 15:06:55 +0100

    New version of github.com/mkrauskopf/devstronomy/
    Changes:
    - 'devstronomy' prefix is not needed anymore. Pages are hosted in the root of
      devstronomy.github.io now.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eaec78abd3ee411ff2d6fb84bbbd809.js"
+  "/devstronomy/precache-manifest.77ac5c9cc99119a93411b8992a45aa2a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/devstronomy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

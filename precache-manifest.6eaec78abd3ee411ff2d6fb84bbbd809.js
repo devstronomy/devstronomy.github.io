ce041b9282ea406f282c02de5dca04a51c855dbb
Martin Krauskopf <martin.krauskopf@gmail.com>
2018-12-18 15:02:02 +0100

    Build of static frontend from github.com/mkrauskopf/devstronomy/

---
+++ precache-manifest.6eaec78abd3ee411ff2d6fb84bbbd809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0aa4eb2619310f858b",
+    "url": "/static/css/main.cacf74bc.chunk.css"
+  },
+  {
+    "revision": "9e0aa4eb2619310f858b",
+    "url": "/static/js/main.9e0aa4eb.chunk.js"
+  },
+  {
+    "revision": "3393ac9e2c822d9f7de4",
+    "url": "/static/js/1.3393ac9e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "debae049da339c3281048b1de21f4df4",
+    "url": "/index.html"
+  }
+];

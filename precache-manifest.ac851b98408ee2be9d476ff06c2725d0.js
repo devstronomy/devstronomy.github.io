b627836d456ba2a3a6e7df3c1d1f0c58fdb84e01
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-01-12 09:09:56 +0100

    Forgotten files.

---
+++ precache-manifest.ac851b98408ee2be9d476ff06c2725d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d1ea7665dfea44797ce",
+    "url": "/static/css/main.db993cbe.chunk.css"
+  },
+  {
+    "revision": "3d1ea7665dfea44797ce",
+    "url": "/static/js/main.3d1ea766.chunk.js"
+  },
+  {
+    "revision": "756d2e0ac8ae6f38dd4e",
+    "url": "/static/js/1.756d2e0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c71944cd842e88a81b52291d78a6e28",
+    "url": "/index.html"
+  }
+];

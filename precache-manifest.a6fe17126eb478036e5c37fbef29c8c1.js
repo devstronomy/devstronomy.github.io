2a10303f876252f9d266733aeb9e225fb19b63d7
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-11-03 14:20:09 +0100

    The new version of github.com/devstronomy/frontend
    Changes:
    - Home page moved to the About
    - Home page are Solar System tables now
    - GitHub icon
    - mainly internal refactorings.

---
+++ precache-manifest.a6fe17126eb478036e5c37fbef29c8c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc991c068246e053a648bf83da37bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a95a8395073c64456b07",
+    "url": "/static/css/main.28321726.chunk.css"
+  },
+  {
+    "revision": "c052b0349adf620df663",
+    "url": "/static/js/2.5b9fd483.chunk.js"
+  },
+  {
+    "revision": "a95a8395073c64456b07",
+    "url": "/static/js/main.2391629a.chunk.js"
+  },
+  {
+    "revision": "a3145de453de11c9e0c3",
+    "url": "/static/js/runtime-main.0aee0bc6.js"
+  }
+]);

08965c31df5d3cfe3bada2f854f439a3bb0afec0
Martin Krauskopf <martin.krauskopf@gmail.com>
2020-09-10 18:02:50 +0200

    The new version of github.com/devstronomy/frontend
    Changes:
    - Introduces Google Analytics

---
+++ precache-manifest.e3d1f4f0d6dd7495d2d392498c45eed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfbfba0e9c5ad65292ebe76be806f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2ae75994304db4bfba",
+    "url": "/static/css/main.10ce0e41.chunk.css"
+  },
+  {
+    "revision": "e89e6fb006e63496b756",
+    "url": "/static/js/2.5b91168e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.5b91168e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2ae75994304db4bfba",
+    "url": "/static/js/main.7cb819e1.chunk.js"
+  },
+  {
+    "revision": "ef885d7e8cc19cac99a6",
+    "url": "/static/js/runtime-main.6dd28220.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0334873360dd3a5ed01b5d4dbf5dcb48.js"
+  "/precache-manifest.e3d1f4f0d6dd7495d2d392498c45eed0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
